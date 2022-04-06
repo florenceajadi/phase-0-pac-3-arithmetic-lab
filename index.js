da376478e4e9685132121b3e42c8fb128f5ad00e
florence ajadi <florence.ajadi@yahoo.com>
2022-04-06 16:32:22 -0500

    commit tree dd0b7d65c738a5905139418e7160f68bc1371aed

---
+++ index.js
@@ -0,0 +1,61 @@
+function add() {
+    return "something";
+  }
+  
+  function subtract() {
+
+  }
+
+  function multiply() {
+
+  }
+
+  function divide() {
+
+  }
+
+  function add(a, b) {
+    return a + b;
+  }
+
+  function subtract(a, b) {
+      return a - b;
+  }
+
+  function multiply(a, b) {
+      return a * b;
+  }
+
+  function divide(a, b) {
+      return a/b;
+  }
+
+  function increment(n) {
+   console.log(n);
+   n++;
+   console.log(n);
+   return n
+ }
+
+ function decrement(n) {
+     console.log(n);
+     n--;
+     console.log(n);
+     return n
+     
+ }
+function makeInt(n) {
+    console.log(n);
+return parseInt(n, 10);
+
+
+}
+
+
+function preserveDecimal(n) {
+    console.log(n);
+    return parseFloat(n);
+   
+}
+
+
